7ddbf37eb566bc5e65ab8c28eccc8adc3f7485f1
Ahmad Sajad Faiz <paghmani.ahmadsajad@gmail.com>
2026-08-16 16:25:25 +0200

    Delete portfolio-project directory

--- portfolio-project/backend/src/main/java/com/example/backend/contact/ContactMessageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.backend.contact;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ContactMessageRepository
-        extends JpaRepository<ContactMessage, Long> {
-}
